8dcbfa079e33620b48761bb35e28f8ca7ff1ed00
kimgura <tjalstl@naver.com>
2020-05-27 17:26:54 +0900

    step01_hello 프로젝트 추가함

---
+++ Step01_Hello/src/test/main/MainClass2.java
@@ -0,0 +1,11 @@
+package test.main;
+
+public class MainClass2 {
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작 되었습니다");
+		System.out.println("하나");
+		System.out.println("두울");
+		System.out.println("세엣");
+		System.out.println("메인 메소드가 리던 됩니당");
+	}
+}
